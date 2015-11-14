e315a432283cf990d8689819e3b8b509e6bbc681
Andrzej Pałkowski <farmerczyna770@gmail.com>
2015-11-14 19:10:16 +0100

    Update API endpoints for playback management

--- app/src/main/java/com/example/andrzej/audiocontroller/config/Endpoints.java
+++ app/src/main/java/com/example/andrzej/audiocontroller/config/Endpoints.java
@@ -13,15 +13,54 @@ public class Endpoints {
     public static final String URL_DATA = DOMAIN + "/data";
     public static final String URL_COVER = DOMAIN + "/file";
     public static final String URL_PLAYLISTS = DOMAIN + "/all_playlists";
+    public static final String URL_PLAY = DOMAIN + "/track/play";
+    public static final String URL_PLAYBACK = DOMAIN + "/track/playback";
+    public static final String URL_PAUSE = DOMAIN + "/track/pause";
+    public static final String URL_UNPAUSE = DOMAIN + "/track/unpause";
+    public static final String URL_REWIND = DOMAIN + "/track/rewind";
+    public static final String URL_STOP = DOMAIN + "/flush";
+    public static final String URL_VOLUME = DOMAIN + "/volume";
 
     //Query Params
     public static final String Q_PATH = "path=";
     public static final String Q_METADATA = "meta=";
     public static final String Q_SORT = "sort=";
     public static final String Q_LOCAL = "local=";
+    public static final String Q_TERMINATE = "t=";
+    public static final String Q_POSITION = "pos=";
+    public static final String Q_UNPAUSE = "unpause=";
 
     //Config
     public static final String CHARSET = "UTF-8";
+
+    public static String getPlayUrl(String path, boolean terminate){
+        String encodedPath = encodeString(path);
+        return URL_PLAY +
+                URL_CHAR_QUESTION +
+                Q_PATH + encodedPath +
+                URL_CHAR_AMEPERSAND +
+                Q_TERMINATE + String.valueOf(terminate);
+    }
+
+    public static String getRewindUrl(int seconds, boolean unpause){
+        return URL_REWIND +
+                URL_CHAR_QUESTION +
+                Q_POSITION + String.valueOf(seconds) +
+                URL_CHAR_AMEPERSAND +
+                Q_UNPAUSE + String.valueOf(unpause);
+    }
+
+    public static String getPlaybackUrl(){
+        return URL_PLAYBACK;
+    }
+
+    public static String getPauseUrl(){
+        return URL_PAUSE;
+    }
+
+    public static String getUnpauseUrl(){
+        return URL_UNPAUSE;
+    }
 
     public static String getDataUrl(String path, boolean withMetadata, int sort) {
         String encodedPath = encodeString(path);
@@ -50,6 +89,14 @@ public class Endpoints {
                 Q_LOCAL + String.valueOf(local) +
                 URL_CHAR_AMEPERSAND +
                 Q_SORT + String.valueOf(sort);
+    }
+
+    public static String getFlushUrl(){
+        return URL_STOP;
+    }
+
+    public static String getVolumeUrl(){
+        return URL_VOLUME;
     }
 
     public static String encodeString(String path) {
