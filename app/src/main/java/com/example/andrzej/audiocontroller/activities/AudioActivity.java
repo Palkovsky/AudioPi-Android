f72111baf49768445adc8fc52848d4442f88a6ad
Andrzej Pałkowski <farmerczyna770@gmail.com>
2015-11-17 22:25:01 +0100

    Set up right drawer for navigating through the playlist

--- app/src/main/java/com/example/andrzej/audiocontroller/activities/AudioActivity.java
+++ app/src/main/java/com/example/andrzej/audiocontroller/activities/AudioActivity.java
@@ -1,8 +1,14 @@
 package com.example.andrzej.audiocontroller.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,7 +22,6 @@ import com.example.andrzej.audiocontroller.models.Track;
 import com.example.andrzej.audiocontroller.utils.Converter;
 import com.example.andrzej.audiocontroller.utils.Image;
 import com.example.andrzej.audiocontroller.views.BlankingImageButton;
-import com.r0adkll.slidr.Slidr;
 import com.squareup.picasso.Picasso;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
@@ -26,9 +31,8 @@ import butterknife.ButterKnife;
 
 public class AudioActivity extends AppCompatActivity implements MediaCallback, DiscreteSeekBar.OnProgressChangeListener, View.OnClickListener {
 
-    //Data
-    private boolean tracking;
-
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
     @Bind(R.id.toolbar_title)
@@ -55,12 +59,10 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
         ButterKnife.bind(this);
-        Slidr.attach(this);
 
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null)
             getSupportActionBar().setTitle(null);
-        toolbarTitle.setSelected(true);
 
         //Listeners
         mainSeekBar.setOnProgressChangeListener(this);
@@ -84,17 +86,17 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
 
     @Override
     public void onMediaRewind(float position) {
-        updateUI();
+        updateUiLight();
     }
 
     @Override
     public void onMediaPause() {
-        updateUI();
+        updateUiLight();
     }
 
     @Override
     public void onMediaUnpause() {
-        updateUI();
+        updateUiLight();
     }
 
     @Override
@@ -104,7 +106,7 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
 
     @Override
     public void onMediaUpdate() {
-        updateUI();
+        updateUiLight();
     }
 
     @Override
@@ -146,7 +148,45 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
             MyApplication.streamManager.rewind(Converter.millisToSeconds(seekBar.getProgress()), true);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.END))
+            mDrawerLayout.closeDrawer(GravityCompat.END);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_audio, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_playlist:
+                if(mDrawerLayout.isDrawerOpen(GravityCompat.END))
+                    mDrawerLayout.closeDrawer(GravityCompat.END);
+                else
+                    mDrawerLayout.openDrawer(GravityCompat.END);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void updateUI() {
+        updateUiLight();
+        Track currentTrack = MyApplication.streamManager.getCurrentTrack();
+        if (currentTrack != null) {
+            toolbarTitle.setText(currentTrack.getFormattedName());
+            toolbarTitle.setSelected(true);
+        }
+    }
+
+    private void updateUiLight() {
         Track currentTrack = MyApplication.streamManager.getCurrentTrack();
         if (currentTrack == null) {
             mainSeekBar.setMin(0);
@@ -154,8 +194,8 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
             mainSeekBar.setProgress(0);
             toolbarTitle.setText("...");
             artistTv.setText("...");
-            totalTimeTv.setText("00:00");
-            currentTimeTv.setText("00:00");
+            totalTimeTv.setText(R.string.zero_formatted_time);
+            currentTimeTv.setText(R.string.zero_formatted_time);
             Image.setSourceDrawable(this, coverIv, R.drawable.default_big_cover);
         } else {
             Metadata metadata = currentTrack.getMetadata();
@@ -165,7 +205,6 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
             mainSeekBar.setMin(0);
             mainSeekBar.setMax(Math.round(currentTrack.getMilliTotalSecs()));
             mainSeekBar.setProgress(Math.round(currentTrack.getMilliPosSecs()));
-            toolbarTitle.setText(currentTrack.getFormattedName());
             artistTv.setText(artist);
             Picasso.with(this).load(currentTrack.getMetadata().getCoverUrl())
                     .placeholder(R.drawable.default_big_cover)
@@ -180,30 +219,30 @@ public class AudioActivity extends AppCompatActivity implements MediaCallback, D
     private void updateButtons() {
         Track currentTrack = MyApplication.streamManager.getCurrentTrack();
         Playlist currentPlaylist = MyApplication.streamManager.getCurrentPlaylist();
-        if(currentTrack == null){
+        if (currentTrack == null) {
             Image.setSourceDrawable(this, playPauseBtn, R.drawable.ic_pause_black_48dp);
             playPauseBtn.setEnabled(false);
             prevBtn.setEnabled(false);
             nextBtn.setEnabled(false);
         }
 
-        if(currentPlaylist == null){
+        if (currentPlaylist == null) {
             prevBtn.setEnabled(false);
             nextBtn.setEnabled(false);
-        }else{
-            if(!currentPlaylist.canGoNext())
+        } else {
+            if (!currentPlaylist.canGoNext())
                 nextBtn.setEnabled(false);
             else
                 nextBtn.setEnabled(true);
-            if(!currentPlaylist.canGoPrev())
+            if (!currentPlaylist.canGoPrev())
                 prevBtn.setEnabled(false);
             else
                 prevBtn.setEnabled(true);
         }
 
-        if(currentTrack != null){
+        if (currentTrack != null) {
             playPauseBtn.setEnabled(true);
-            if(currentTrack.isPaused())
+            if (currentTrack.isPaused())
                 Image.setSourceDrawable(this, playPauseBtn, R.drawable.ic_play_arrow_black_48dp);
             else
                 Image.setSourceDrawable(this, playPauseBtn, R.drawable.ic_pause_black_48dp);
